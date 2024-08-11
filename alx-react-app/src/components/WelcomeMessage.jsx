1301d66845e70ff8e7606bc8ef57a190f613d4e9
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-08-11 20:46:38 +0200

    modified containing files

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,9 +1,9 @@
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello Everyone, I am learning react at ALX!</h1>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>This is a simple JSX component.</p>
-            <P>I am learning about JSX</P>
+            <p>I am learning about JSX</p>
             
         </div>
     );
