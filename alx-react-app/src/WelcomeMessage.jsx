70604a6719ba14abbd89930ba397c25711495865
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-08-11 20:28:05 +0200

    added all requirements

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello Everyone, I am learning react at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <P>I am learning about JSX</P>
+            
+        </div>
+    );
+}
+
+export default WelcomeMessage;
