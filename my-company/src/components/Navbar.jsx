384f0c5cddc22c9a1b3eeaa63398d007bce40d81
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-08-18 23:48:59 +0200

    modified nav styles

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,13 +1,36 @@
-// Navbar.jsx
+// src/components/Navbar.jsx
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
-      <Link to="/" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>Contact</Link>
+    <nav style={{ 
+      padding: '10px', 
+      backgroundColor: '#333', 
+      color: '#fff', 
+      display: 'flex', 
+      justifyContent: 'center', // Center the items horizontally
+      alignItems: 'center' // Center the items vertically
+    }}>
+      <Link to="/" style={{ 
+        margin: '0 15px', 
+        color: '#fff', 
+        textDecoration: 'none' 
+      }}>Home</Link>
+      <Link to="/about" style={{ 
+        margin: '0 15px', 
+        color: '#fff', 
+        textDecoration: 'none' 
+      }}>About</Link>
+      <Link to="/services" style={{ 
+        margin: '0 15px', 
+        color: '#fff', 
+        textDecoration: 'none' 
+      }}>Services</Link>
+      <Link to="/contact" style={{ 
+        margin: '0 15px', 
+        color: '#fff', 
+        textDecoration: 'none' 
+      }}>Contact</Link>
     </nav>
   );
 }
