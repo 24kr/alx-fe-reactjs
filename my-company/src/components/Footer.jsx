f334f822bfa4f2edf3bcbc2c02d20da80d27e565
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-08-18 23:43:29 +0200

    All set

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// Footer.jsx
+function Footer() {
+  return (
+    <footer style={{ padding: '20px', backgroundColor: '#333', color: '#fff', textAlign: 'center', marginTop: '20px' }}>
+      <p>&copy; 2024 Our Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
