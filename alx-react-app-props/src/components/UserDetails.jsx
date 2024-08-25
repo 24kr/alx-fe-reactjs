284cd0d300b24858095c48e7252f9804e6bb90c8
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-08-25 23:25:38 +0200

    changed UserContext repo

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
-import UserContext from "../UserContext/UserContext";
+import UserContext from "./UserContext";
 
-function UserDetails() {
+function UserDetails()) {
     const userData = useContext(UserContext)
   return (
     <div>
