a4f3c6f27154ae7b8c0e15e84104222fddbad379
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2024-09-09 05:54:52 +0200

    modified

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  pruge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Purge unused styles
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Purge unused styles
   darkMode: false, // Or 'media' or 'class'
   theme: {
     extend: {},
